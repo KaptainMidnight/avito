87c9e0f62d2e5fa3a8ff8f70f260c71bae04ab6a
artemkin <aartemkin030@gmail.com>
2023-12-10 20:27:25 +0400

    Added base user fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'phone',
         'email',
         'password',
     ];
@@ -40,6 +42,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
